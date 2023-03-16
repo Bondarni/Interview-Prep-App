022c0fb06e544a630784621cd0ebf9100d1d6460
Michael Durando <mjdurando82@yahoo.com>
2023-03-16 19:03:03 -0400

    student details page grabs info

--- client/src/pages/StudentDetails.vue
+++ client/src/pages/StudentDetails.vue
@@ -1,19 +1,34 @@
 <template>
     <div>
-      <h1>Student Name</h1>
+      <h1>{{ studentDetails.name }}</h1>
       <h3>Overall GPA</h3>
       <h4>List of Courses with associated letter grade</h4>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
   export default{
     name: 'StudentDetails',
     components: {},
-    data: () => ({}),
-    mounted() {},
-    methods: {}
+    data: () => ({
+      studentDetails: {}
+    }),
+    mounted() {
+      this.getStudentDetails()
+    },
+    methods: {
+      async getStudentDetails() {
+        const studentId = this.$route.params.student_id
+        const response = await axios.get(`http://localhost:3001/api/students/${studentId}`)
+        this.studentDetails = response.data
+      },
+      goHome(){
+        this.$router.push(`/`)
+    }
   }
+}
 </script>
 
 <style>
