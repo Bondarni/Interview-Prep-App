4ef7c60a090817b9a5116532a82be6cf1fa40d90
Michael Durando <mjdurando82@yahoo.com>
2023-03-16 14:57:40 -0400

    student controller and router updated

---
+++ routes/StudentRouter.js
@@ -0,0 +1,16 @@
+const Router = require('express').Router()
+const controller = require('../controllers/StudentController')
+const middleware = require('../middleware')
+
+Router.get('/all', controller.GetAllStudents)
+
+Router.get('/:id', controller.GetStudentByPk)
+
+Router.post(
+  '/newstudent',
+  // middleware.stripToken,
+  // middleware.verifyToken,
+  controller.CreateStudent
+)
+
+module.exports = Router
