51cef34f577c4cd9f311e66bbcc809cad5a22568
Anatoliy Agadzhanov <aanatoliy17@gmail.com>
2023-03-16 14:34:03 -0400

    fixed association between grade and course, started auth router and controller

--- models/grade.js
+++ models/grade.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      Grade.belongsToMany(models.Course, {
+      Grade.hasMany(models.Course, {
         foreignKey: 'gradeId'
       })
     }
