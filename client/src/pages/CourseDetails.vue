68552107989d941e1c4a17424d29276767ed0631
Michael Durando <mjdurando82@yahoo.com>
2023-03-17 11:30:31 -0400

    updated course details

--- client/src/pages/CourseDetails.vue
+++ client/src/pages/CourseDetails.vue
@@ -3,7 +3,11 @@
     <h1>{{ courseDetails.name }}</h1>
     <div v-for="student in students" :key="student.id">
       <h5>{{ student.name }}</h5>
-      <h5> Grade: {{ student.Student_Course.grade }}</h5>
+      <h5 v-if="student.Student_Course.grade === 4">Grade: A</h5>
+      <h5 v-else-if="student.Student_Course.grade === 3">Grade: B</h5>
+      <h5 v-else-if="student.Student_Course.grade === 2">Grade: C</h5>
+      <h5 v-else-if="student.Student_Course.grade === 1">Grade: D</h5>
+      <h5 v-else-if="student.Student_Course.grade === 0">Grade: F</h5>
     </div>
   </div>
   <button @click="goHome">Back</button>
@@ -31,7 +35,6 @@ import Client from '../services/api'
         const courseId = this.$route.params.course_id
         const response = await axios.get(`http://localhost:3001/api/courses/${courseId}`)
         this.courseDetails = response.data
-        console.log(this.courseDetails)
       },
       async getStudents() {
         const courseId = this.$route.params.course_id
